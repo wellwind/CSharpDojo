2348561c02a18108de1662cdda207b97772ebe87
wellwind <wellwind@mail2000.com.tw>
2016-09-14 19:44:56 +0800

    init project

---
+++ SimpleEncryption_AlternatingSplit/SimpleEncryption_AlternatingSplit/AlternatingSplit.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleEncryption_AlternatingSplit
+{
+    public class AlternatingSplit
+    {
+        public static string Encrypt(string text, int n)
+        {
+            return text;
+        }
+
+        public static string Decrypt(string encryptedText, int n)
+        {
+            return encryptedText;
+        }
+    }
+}
---
+++ SimpleEncryption_AlternatingSplit/SimpleEncryption_AlternatingSplitTests/KataTests.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SimpleEncryption_AlternatingSplit;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleEncryption_AlternatingSplit.Tests
+{
+    [TestClass()]
+    public class KataTests
+    {
+        [TestMethod()]
+        public void EncryptTest()
+        {
+            Assert.AreEqual("This is a test!", AlternatingSplit.Encrypt("This is a test!", 0));
+            Assert.AreEqual("hsi  etTi sats!", AlternatingSplit.Encrypt("This is a test!", 1));
+            Assert.AreEqual("s eT ashi tist!", AlternatingSplit.Encrypt("This is a test!", 2));
+            Assert.AreEqual(" Tah itse sits!", AlternatingSplit.Encrypt("This is a test!", 3));
+            Assert.AreEqual("This is a test!", AlternatingSplit.Encrypt("This is a test!", 4));
+            Assert.AreEqual("This is a test!", AlternatingSplit.Encrypt("This is a test!", -1));
+            Assert.AreEqual("hskt svr neetn!Ti aai eyitrsig", AlternatingSplit.Encrypt("This kata is very interesting!", 1));
+        }
+    }
+}
