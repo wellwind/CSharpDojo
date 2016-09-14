4d221e9e62ec9fd4a08ec43f614b4f855e64e2ce
wellwind <wellwind@mail2000.com.tw>
2016-09-14 20:30:16 +0800

    Init Project

---
+++ PersistentBuggerKata/PersistentBuggerKata/Kata.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PersistentBuggerKata
+{
+    public class Kata
+    {
+        public static int Persistence(long n)
+        {
+            return 0;
+        }
+    }
+}
---
+++ PersistentBuggerKata/PersistentBuggerKataTests/KataTests.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PersistentBuggerKata;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PersistentBuggerKata.Tests
+{
+    [TestClass()]
+    public class KataTests
+    {
+        [TestMethod()]
+        public void PersistenceTest()
+        {
+            Assert.AreEqual(3, Kata.Persistence(39));
+            Assert.AreEqual(0, Kata.Persistence(4));
+            Assert.AreEqual(2, Kata.Persistence(25));
+            Assert.AreEqual(4, Kata.Persistence(999));
+        }
+    }
+}
