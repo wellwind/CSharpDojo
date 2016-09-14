0ac28f11d46100e1fd070b2fc3a3adbb6846f34d
wellwind <wellwind@mail2000.com.tw>
2016-09-14 20:05:18 +0800

    Finish Kata

--- WordA10nKata/WordA10nKata/Kata.cs
+++ WordA10nKata/WordA10nKata/Kata.cs
@@ -10,7 +10,33 @@ namespace WordA10nKata
     {
         public static string Abbreviate(string input)
         {
-            return "";
+            var result = "";
+            var tmp = "";
+            foreach (var ch in input)
+            {
+                if ((ch >= 'a' && ch <= 'z') || (ch >= 'A' && ch <= 'Z'))
+                {
+                    tmp += ch.ToString();
+                }
+                else
+                {
+                    result += abbreviateWord(tmp) + ch.ToString();
+                    tmp = "";
+                }
+            }
+            return result + abbreviateWord(tmp);
+        }
+
+        private static string abbreviateWord(string input)
+        {
+            if (input.Length <= 3)
+            {
+                return input;
+            }
+            else
+            {
+                return String.Format("{0}{1}{2}", input.First(), input.Length - 2, input.Last());
+            }
         }
     }
 }
