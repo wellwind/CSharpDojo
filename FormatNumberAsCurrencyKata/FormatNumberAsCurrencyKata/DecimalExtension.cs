8114105852c5ba2984264c9330f649ecf6b1cb43
wellwind <wellwind@mail2000.com.tw>
2016-09-14 19:50:42 +0800

    Init Project

---
+++ FormatNumberAsCurrencyKata/FormatNumberAsCurrencyKata/DecimalExtension.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FormatNumberAsCurrencyKata
+{
+    public static class DecimalExtension
+    {
+        public static string ToCurrency(this Decimal input, string prefix)
+        {
+            return String.Empty;
+        }
+    }
+}
---
+++ FormatNumberAsCurrencyKata/FormatNumberAsCurrencyKataTests/DecimalExtensionTests.cs
@@ -0,0 +1,44 @@
+﻿using FormatNumberAsCurrencyKata;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FormatNumberAsCurrencyKata.Tests
+{
+    [TestClass()]
+    public class DecimalExtensionTests
+    {
+        [TestMethod]
+        public void When0ToDollarsReturnsDollar0_00()
+        {
+            Assert.AreEqual("$0.00", (0m).ToCurrency("$"));
+        }
+
+        [TestMethod]
+        public void When1_23ToDollarsReturnsDollar1_23()
+        {
+            Assert.AreEqual("$1.23", (1.23m).ToCurrency("$"));
+        }
+
+        [TestMethod]
+        public void When99_9999ToDollarsReturnsDollar100_00()
+        {
+            Assert.AreEqual("$100.00", (99.9999m).ToCurrency("$"));
+        }
+
+        [TestMethod]
+        public void WhenNegative12345_6789ToDollarsReturnsMinusDollar12345__68()
+        {
+            Assert.AreEqual("-$12345.68", (-12345.6789m).ToCurrency("$"));
+        }
+
+        [TestMethod]
+        public void When12345_6789ToPoundsReturnsPound12345__68()
+        {
+            Assert.AreEqual("£12345.68", (12345.6789m).ToCurrency("£"));
+        }
+    }
+}
