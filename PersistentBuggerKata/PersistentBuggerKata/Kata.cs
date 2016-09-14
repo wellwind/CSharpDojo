2e260da69165253482300ac7d0da9226ac85168e
wellwind <wellwind@mail2000.com.tw>
2016-09-14 20:30:52 +0800

    Finish Kata

--- PersistentBuggerKata/PersistentBuggerKata/Kata.cs
+++ PersistentBuggerKata/PersistentBuggerKata/Kata.cs
@@ -10,7 +10,20 @@ namespace PersistentBuggerKata
     {
         public static int Persistence(long n)
         {
-            return 0;
+            var str = n.ToString();
+            var cnt = 0;
+
+            while (str.Length > 1)
+            {
+                ++cnt;
+                long num = 1;
+                foreach (var ch in str.ToCharArray())
+                {
+                    num *= Convert.ToInt64(ch.ToString());
+                }
+                str = num.ToString();
+            }
+            return cnt;
         }
     }
 }
