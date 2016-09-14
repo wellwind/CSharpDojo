28b63ee687df7b051af8db97e2cec687ee17df72
wellwind <wellwind@mail2000.com.tw>
2016-09-14 19:11:10 +0800

    finish VowelShiftingKata

--- VowelShiftingKata/VowelShiftingKataTests/KataTests.cs
+++ VowelShiftingKata/VowelShiftingKataTests/KataTests.cs
@@ -15,8 +15,30 @@ namespace VowelShiftingKata.Tests
         public void VowelShiftTest()
         {
             Assert.AreEqual("This is a test!", Kata.VowelShift("This is a test!", 0));
+        }
+
+        [TestMethod()]
+        public void VowelShiftOnceTest()
+        {
             Assert.AreEqual("Thes is i tast!", Kata.VowelShift("This is a test!", 1));
+        }
+
+        [TestMethod()]
+        public void VowelShift3TimesTest()
+        {
             Assert.AreEqual("This as e tist!", Kata.VowelShift("This is a test!", 3));
+        }
+
+        [TestMethod()]
+        public void VowelShiftNegativeThreeTimesTest()
+        {
+            Assert.AreEqual("Thes is i tast!", Kata.VowelShift("This is a test!", -3));
+        }
+
+        [TestMethod()]
+        public void VowelShiftUpperCaseTest()
+        {
+            Assert.AreEqual("e Appla!", Kata.VowelShift("A apple!", 1));
         }
     }
 }
