f721e506d7c8720f7ffa073d6ccb11a7be3ac29c
wellwind <wellwind@mail2000.com.tw>
2016-09-14 20:23:32 +0800

    Init Project

---
+++ DuplicateEncoderKata/DuplicateEncoderKata/Class1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DuplicateEncoderKata
+{
+    public class Kata
+    {
+        public static string DuplicateEncode(string word)
+        {
+            return word;
+        }
+    }
+}
---
+++ DuplicateEncoderKata/DuplicateEncoderKataTests/KataTests.cs
@@ -0,0 +1,23 @@
+﻿using DuplicateEncoderKata;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DuplicateEncoderKata.Tests
+{
+    [TestClass()]
+    public class KataTests
+    {
+        [TestMethod()]
+        public void DuplicateEncodeTest()
+        {
+            Assert.AreEqual("(((", Kata.DuplicateEncode("din"));
+            Assert.AreEqual("()()()", Kata.DuplicateEncode("recede"));
+            Assert.AreEqual(")())())", Kata.DuplicateEncode("Success"), "should ignore case");
+            Assert.AreEqual("))((", Kata.DuplicateEncode("(( @"));
+        }
+    }
+}
