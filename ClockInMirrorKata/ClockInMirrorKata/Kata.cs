61c5f919ef5884a38ce0d1793263c1f272d20492
wellwind <wellwind@mail2000.com.tw>
2016-09-14 20:19:33 +0800

    Finish Kata

--- ClockInMirrorKata/ClockInMirrorKata/Kata.cs
+++ ClockInMirrorKata/ClockInMirrorKata/Kata.cs
@@ -10,7 +10,28 @@ namespace ClockInMirrorKata
     {
         public static string WhatIsTheTime(string timeInMirror)
         {
-            return "";
+            var hour = Convert.ToInt32(timeInMirror.Substring(0, 2));
+            var minute = Convert.ToInt32(timeInMirror.Substring(3, 2));
+
+            if (minute == 0)
+            {
+                hour = 12 - hour;
+            }
+            else
+            {
+                hour = 11 - hour;
+                if (hour < 0)
+                {
+                    hour = 12 + hour;
+                }
+                minute = 60 - minute;
+            }
+
+            if (hour == 0)
+            {
+                hour = 12;
+            }
+            return String.Format("{0}:{1}", hour.ToString("00"), minute.ToString("00"));
         }
     }
 }
