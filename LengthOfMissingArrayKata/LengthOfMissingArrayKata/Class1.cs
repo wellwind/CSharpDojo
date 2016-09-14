e9660f9d64dd1c9e4adea29e696a893a20f79e85
wellwind <wellwind@mail2000.com.tw>
2016-09-14 19:58:03 +0800

    Init Project

---
+++ LengthOfMissingArrayKata/LengthOfMissingArrayKata/Class1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LengthOfMissingArrayKata
+{
+    public class Kata
+    {
+        public static int GetLengthOfMissingArray(object[][] arrayOfArrays)
+        {
+            return 0;
+        }
+    }
+}
