03c8b99b7b7871a1681490659b59f601a6df3c82
wellwind <wellwind@mail2000.com.tw>
2016-09-14 19:58:38 +0800

    Finish Kata

---
+++ LengthOfMissingArrayKata/LengthOfMissingArrayKata/Kata.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LengthOfMissingArrayKata
+{
+    public class Kata
+    {
+        public static int GetLengthOfMissingArray(object[][] arrayOfArrays)
+        {
+            if (arrayOfArrays == null)
+            {
+                return 0;
+            }
+
+            var lengthArray = arrayOfArrays
+              .Select(subArray => subArray == null ? 0 : subArray.Count())
+              .OrderBy(len => len)
+              .ToArray();
+
+            if (lengthArray.Any(len => len == 0))
+            {
+                return 0;
+            }
+
+            for (var i = 1; i <= lengthArray.Count(); ++i)
+            {
+                if ((lengthArray[i] - lengthArray[i - 1]) > 1)
+                {
+                    return lengthArray[i - 1] + 1;
+                }
+            }
+            return 0;
+        }
+    }
+}
