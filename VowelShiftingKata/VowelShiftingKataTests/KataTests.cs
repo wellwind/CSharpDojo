71e6e6e03a3066f0d5acb4b5dddd6f926de08769
wellwind <wellwind@mail2000.com.tw>
2016-09-14 18:21:40 +0800

    Init project

---
+++ VowelShiftingKata/VowelShiftingKataTests/KataTests.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VowelShiftingKata;
+
+namespace VowelShiftingKata.Tests
+{
+    [TestClass()]
+    public class KataTests
+    {
+        [TestMethod()]
+        public void VowelShiftTest()
+        {
+            Assert.AreEqual("This is a test!", Kata.VowelShift("This is a test!", 0));
+            Assert.AreEqual("Thes is i tast!", Kata.VowelShift("This is a test!", 1));
+            Assert.AreEqual("This as e tist!", Kata.VowelShift("This is a test!", 3));
+        }
+    }
+}
