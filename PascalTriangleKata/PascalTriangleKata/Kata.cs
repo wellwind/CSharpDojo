a406f0bc6fc3b84122a2e368cd4f65d34e78d44a
wellwind <wellwind@mail2000.com.tw>
2016-09-16 22:21:47 +0800

    Init Project

---
+++ PascalTriangleKata/PascalTriangleKata/Kata.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PascalTriangleKata
+{
+    public class Kata
+    {
+        public static List<int> PascalsTriangle(int n)
+        {
+            return new List<int>() { 1 };
+        }
+    }
+}
---
+++ PascalTriangleKata/PascalTriangleKataTests/KataTests.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PascalTriangleKata;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PascalTriangleKata.Tests
+{
+    [TestClass()]
+    public class KataTests
+    {
+        [TestMethod()]
+        public void PascalsTriangleTest()
+        {
+            CollectionAssert.AreEqual(
+                new List<int> {1},
+                Kata.PascalsTriangle(1));
+        }
+    }
+}
