5dda53252c83ece9be57e33060d7833f2cc87038
wellwind <wellwind@mail2000.com.tw>
2016-09-16 22:35:13 +0800

    Finish Kata

--- PascalTriangleKata/PascalTriangleKata/Kata.cs
+++ PascalTriangleKata/PascalTriangleKata/Kata.cs
@@ -10,7 +10,27 @@ namespace PascalTriangleKata
     {
         public static List<int> PascalsTriangle(int n)
         {
-            return new List<int>() { 1 };
+            if (n == 1) return new List<int>() { 1 };
+            if (n == 2) return new List<int>() { 1, 1 };
+
+            var result = new List<int>() {1, 1, 1};
+            var tmp = new List<int>() { 1, 1 };
+
+            for (var i = 3; i <= n; ++i)
+            {
+                var currentLevelData = new List<int>();
+
+                currentLevelData.Add(1);
+                for (var j = 1; j <= i - 2; ++j)
+                {
+                    currentLevelData.Add(tmp[j-1] + tmp[j]);
+                }
+                currentLevelData.Add(1);
+
+                result.AddRange(currentLevelData);
+                tmp = currentLevelData;
+            }
+            return result;
         }
     }
 }
--- PascalTriangleKata/PascalTriangleKataTests/KataTests.cs
+++ PascalTriangleKata/PascalTriangleKataTests/KataTests.cs
@@ -15,8 +15,22 @@ namespace PascalTriangleKata.Tests
         public void PascalsTriangleTest()
         {
             CollectionAssert.AreEqual(
-                new List<int> {1},
+                new List<int> { 1 },
                 Kata.PascalsTriangle(1));
+
+            CollectionAssert.AreEqual(
+                new List<int> { 1, 1 },
+                Kata.PascalsTriangle(2));
+        }
+
+        [TestMethod]
+        public void PascalsTriangleTestWhenInput4()
+        {
+            var expected = new List<int> { 1, 1, 1, 1, 2, 1, 1, 3, 3, 1 };
+
+            var actual = Kata.PascalsTriangle(4);
+
+            CollectionAssert.AreEqual(expected, actual);
         }
     }
 }
